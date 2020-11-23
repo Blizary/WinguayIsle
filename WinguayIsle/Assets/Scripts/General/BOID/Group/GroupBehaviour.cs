f66f9a554f4c682d2c50de9a905dfc55c812f1be
blizary <rosajunior05@gmail.com>
2020-11-23 03:33:13 +0000

    Base project and boids start

---
+++ WinguayIsle/Assets/Scripts/General/BOID/Group/GroupBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// this automaticly adds a spherecollider when its used
+// otherwise it will atleast provide an error
+[RequireComponent(typeof(CircleCollider2D))]
+
+public abstract class GroupBehaviour : SteeringBehaviour
+{
+	protected List<Vehicle> neighbours = new List<Vehicle>();
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.CompareTag(gameObject.tag))
+		{
+			Vehicle vehicle = c.GetComponent<Vehicle>();
+			neighbours.Add(vehicle);
+
+		}
+	}
+
+
+	void OnTriggerExit(Collider c)
+	{
+		if (c.gameObject.CompareTag(gameObject.tag))
+		{
+			Vehicle vehicle = c.GetComponent<Vehicle>();
+			neighbours.Remove(vehicle);
+
+		}
+	}
+
+}
