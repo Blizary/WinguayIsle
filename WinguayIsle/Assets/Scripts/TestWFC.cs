a23c5e7c78df447740e846d696a14afa4bdf636f
blizary <rosajunior05@gmail.com>
2021-01-05 10:51:58 +0000

    Auto stash before merge of "main" and "origin/main"

---
+++ WinguayIsle/Assets/Scripts/TestWFC.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using WaveFunctionCollapse;
+
+public class TestWFC : MonoBehaviour
+{
+    public Tilemap input; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputReader reader = new InputReader(input);
+        var grid = reader.ReadInputToGrid();
+        /*
+        for (int row = 0; row < grid.Length; row++)
+        {
+            for (int col = 0; col < grid[0].Length; col++)
+            {
+                Debug.Log("Row " + row + " Col " + col + " tile name " + grid[row][col].Value.name);
+            }
+        }
+        */
+
+        ValuesManager<TileBase> valueManager = new ValuesManager<TileBase>(grid);
+        StringBuilder builder = null;
+        List<string> list = new List<string>();
+        for (int row = 0; row < grid.Length; row++)
+        {
+            builder = new StringBuilder();
+            for (int col = 0; col < grid[0].Length; col++)
+            {
+                builder.Append(valueManager.GetGridValuesIncludingOffset(col, row) + " ");
+            }
+            list.Add(builder.ToString());
+        }
+
+        list.Reverse();
+        foreach( var item in list)
+        {
+            Debug.Log(item);
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WinguayIsle/Assets/Scripts/WFC/Input/TileContainer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace WaveFunctionCollapse
+{
+    public class TileContainer
+    {
+        public TileBase Tile { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public TileContainer(TileBase _tile, int _x, int _y)
+        {
+            this.Tile = _tile;
+            this.X = _x;
+            this.Y = _y;
+        }
+    }
+
+}
